2b623961b6f3a7e2c0f017b9221f0d0cc9ee67d5
Sanso95 <marco.sansoni.1995@gmail.com>
2020-03-01 21:22:05 +0100

    solved bug on first time printed

--- src/TimePicker/TimePicker.js
+++ src/TimePicker/TimePicker.js
@@ -49,7 +49,7 @@ const TimePicker = ({
   // Need to decide how to do with hour
   // Store always into 24h format and display different according with the choice made
   const [currentHour, setCurrentHour] = useState(moment());
-  const [isAM, setIsAm] = useState(true);
+  const [isAM, setIsAm] = useState(currentHour.format('HH') <= 12);
 
   // hours
   let itemsHour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -60,32 +60,40 @@ const TimePicker = ({
   // console.log(itemsHour);
 
   const onChangeFromDropdown = (type, value) => {
-    console.log({ type, value });
+    // console.log({ type, value });
+    // const temporaryHour = moment(currentHour.format('HH:mm'), 'HH:mm');
+    const temporaryHour = moment(currentHour);
     switch (type) {
       case 'hour':
         // console.log(isAM);
         // setCurrentHour(moment().set('hour', isAM ? value : value + 12));
-        setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('hour', isAM ? value : value + 12));
+        // setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('hour', isAM ? value : value + 12));
+        temporaryHour.set('hour', isAM ? value : value + 12);
         break;
       case 'minute':
-        setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('minute', value));
+        // setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('minute', value));
+        temporaryHour.set('minute', value);
         break;
       case 'twelveHours':
         // console.log('AMMNAMAMA');
         if (value === 'AM') {
           setIsAm(true);
-          setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('hour', currentHour.hour() - 12));
+          // setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('hour', currentHour.hour() - 12));
+          temporaryHour.set('hour', currentHour.hour() - 12);
         }
         // console.log('set to false');
         if (value === 'PM') {
           setIsAm(false);
-          setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('hour', currentHour.hour() + 12));
+          // setCurrentHour(moment(currentHour.format('HH:mm'), 'HH:mm').set('hour', currentHour.hour() + 12));
+          temporaryHour.set('hour', currentHour.hour() + 12);
         }
         break;
       default:
         break;
     }
-    onChange(currentHour);
+    setCurrentHour(temporaryHour);
+    // console.log(temporaryHour.format('HH:mm'));
+    onChange(temporaryHour);
   };
 
 
@@ -99,17 +107,20 @@ const TimePicker = ({
           onChange={(h) => onChangeFromDropdown('hour', h)}
           type="hour"
           items={itemsHour}
+          defaultIndex={currentHour.format('h') - 1}
         />
         <Dropdown
           type="minute"
           onChange={(h) => onChangeFromDropdown('minute', h)}
           items={Array.from(Array(60).keys())}
+          defaultIndex={currentHour.format('mm')}
         />
         {TwelveHours && (
         <Dropdown
           type="twelveHours"
           onChange={(h) => onChangeFromDropdown('twelveHours', h)}
           items={['AM', 'PM']}
+          defaultIndex={currentHour.format('HH') > 12 && 1}
         />
         )}
       </Selector>
